ad1ac56ce2d7f1146cddcbad596f915ddcad6cf1
Stella Jiang <stellaj@Stellas-MacBook-Pro.local>
2016-06-27 23:37:28 -0400

    removed queries and constants from for loops in changePizzaSize function

--- views/js/main.js
+++ views/js/main.js
@@ -449,10 +449,10 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var c = document.querySelectorAll(".randomPizzaContainer");
+    var newWidth = (sizeSwitcher(size) * 1170) + 'px';
+    for (var i = 0; i < c.length; i++) {
+      c[i].style.width = newWidth;
     }
   }
 
